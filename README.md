1ea12787047b7c745d473dbb791bd4804d6c3fef
0i0 <liorhakim@gmail.com>
2012-10-04 10:53:34 +0200

    Initial

---
+++ README.md
@@ -0,0 +1,7 @@
+Base Node on Heroku
+========
+
+is a live Api buffer
+
+so basiclly buffy gets a periodic input of current data from an external element.
+than when requested she can squirt long streams of past incidents from her memory
---
+++ config.js
@@ -0,0 +1,15 @@
+var Config = module.exports = function Config(debug) {
+  var config = {
+    dev :   {debug : true
+            ,orign : 'http://0.0.0.0:8000'
+            ,port:8000
+            ,sessionSecret : '50m35ecr3t'
+            },
+    prod :  {debug : false
+            ,orign : 'http://chordify.herokuapp.com'
+            ,port:process.env.PORT
+            ,sessionSecret : '50m35ecr3t'
+            }
+  }
+  return config[(debug)?'dev':'prod'];
+}
